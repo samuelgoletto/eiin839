dac53e3b2429da8f606873d45ead50be594afd76
Samuel-Goletto <samuelgoletto@hotmail.fr>
2022-03-29 15:24:07 +0200

    Rendu FINAL

--- TD6/src/ClientSOAP/TD2WebServices/Program.cs
+++ TD6/src/ClientSOAP/TD2WebServices/Program.cs
@@ -11,27 +11,30 @@ namespace TD2WebServices
         static void Main(string[] args)
         {
             //Tests du seveur SOAP
-            //Appuyer sur la toucher ENTER pour passer à l'opéaration suivante
             ServiceReference2.MathsOperationsClient calculator = new ServiceReference2.MathsOperationsClient();
 
             int res = calculator.Add(3, 2);
             Console.WriteLine("Calcul de 3 + 2 :");
             Console.WriteLine(res);
+            Console.WriteLine("Appuyer sur la toucher ENTER pour passer à l'opération suivante");
             Console.ReadKey();
 
             res = calculator.Substract(3, 2);
             Console.WriteLine("Calcul de 3 - 2 :");
             Console.WriteLine(res);
+            Console.WriteLine("Appuyer sur la toucher ENTER pour passer à l'opération suivante");
             Console.ReadKey();
 
             res = calculator.Multiply(3, 2);
             Console.WriteLine("Calcul de 3 * 2 :");
             Console.WriteLine(res);
+            Console.WriteLine("Appuyer sur la toucher ENTER pour passer à l'opération suivante");
             Console.ReadKey();
 
             float res2 = calculator.Divide(3.0f, 2.0f);
             Console.WriteLine("Calcul de 3 / 2 :");
             Console.WriteLine(res2);
+            Console.WriteLine("Fin des tests SOAP. Pour tester le serveur REST, merci d'utiliser Postman (informations dans le readme)");
             Console.ReadKey();
         }
     }
